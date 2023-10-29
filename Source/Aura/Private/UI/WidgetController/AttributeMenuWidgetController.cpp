112df6cd73d83eca9fb1356b61beed50d215ab64
Ruben Montes <ruben.montes.mendoza@gmail.com>
2023-10-29 16:18:15 +0100

    96

--- Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -9,7 +9,19 @@
 
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 {
-	
+	UAuraAttributeSet* AS = CastChecked<UAuraAttributeSet>(AttributeSet);
+
+	for (auto& Pair : AS->TagsToAttributes)
+	{
+		AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(Pair.Value()).AddLambda(
+			[this, Pair, AS](const FOnAttributeChangeData& Data)
+		{
+			FAuraAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(Pair.Key);
+			Info.AttributeValue = Pair.Value().GetNumericValue(AS);
+			AttributeInfoDelegate.Broadcast(Info);
+		}
+		);
+	}
 }
 
 void UAttributeMenuWidgetController::BroadcastInitialValues()
